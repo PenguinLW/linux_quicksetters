d389ebb253d78a4a5a5f1feb993f2f416a1186bd
PenguinL <diana1997525@gmail.com>
2020-08-08 23:05:23 +0800

    Update escritorio_get.sh

--- escritorio_get.sh
+++ escritorio_get.sh
@@ -1,2 +1,3 @@
+#chmod +x escritorio_get.sh && ./escritorio_get.sh
 #tap for get windows-desktop directory from current
 ln -s /path/todrive/windows/users/username/Desktop /home/username/currentdesktop
