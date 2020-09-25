079f8f59d82f83b64c90179b1bb75bd05a5125fd
PenguinL <diana1997525@gmail.com>
2020-09-25 23:07:21 +0800

    Create set_ttl.sh

---
+++ set_ttl/set_ttl.sh
@@ -0,0 +1,20 @@
+echo "echo 119 > /proc/sys/net/ipv4/ip_default_ttl & rm ttl.sh" > ttl.sh
+chmod +x ttl.sh
+sudo ./ttl.sh
+#1) sudo gedit /root/ttl.sh
+#2) 
+#	a) writing:
+#		echo 119 > /proc/sys/net/ipv4/ip_default_ttl
+#	-b) checked:
+#		systemctl status NetworkManager.service
+#-3) sudo gedit /etc/systemd/system/ttl.service
+#-4) writing:
+#[Unit]
+#Description=TTL119
+#Before=NetworkManager.service
+#
+#[Service]
+#User=root
+#RemainAfterExit=yes
+#Type=oneshot
+#5) sudo systemctl enable ttl.service
