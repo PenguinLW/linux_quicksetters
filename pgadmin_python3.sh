29afae80ba501922e2fbb7d8413ef792afe2e0d5
PenguinL <diana1997525@gmail.com>
2020-10-09 00:16:55 +0800

    Update pgadmin_python3.sh

--- pgadmin_python3.sh
+++ pgadmin_python3.sh
@@ -1,2 +1,9 @@
-sudo pip3 install setuptools
-sudo pip3 install https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.23/pip/pgadmin4-4.23-py3-none-any.whl --ignore-installed
+#sudo apt-get install python3-venv
+#sudo python3 -m venv pgadmin
+#source pgadmin/bin/activate
+#sudo pip3 install setuptools
+sudo apt-get install python3.5-dev -y
+#pip3 install https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.23/pip/pgadmin4-4.23-py3-none-any.whl --ignore-installed
+sudo pip3 install https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.26/pip/pgadmin4-4.26-py3-none-any.whl --ignore-installed
+python3 ~/pgadmin4/lib/python3.8/site-packages/pgadmin4/pgAdmin4.py
+#deactivate
