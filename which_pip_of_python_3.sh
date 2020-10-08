b402234da53ac3b776f82a1aac338230d16ad7f7
PenguinL <diana1997525@gmail.com>
2020-10-08 22:56:11 +0800

    Update which_pip_of_python_3.sh

--- which_pip_of_python_3.sh
+++ which_pip_of_python_3.sh
@@ -1,8 +1,11 @@
-sudo apt install python3-pip -y
+
+sudo apt install python-pip -y
 which python
 python -V
+which pip
+pip -V
+sudo apt install python3-pip -y
 which python3
 python3 -V
-which pip
 which pip3
 pip3 -V
