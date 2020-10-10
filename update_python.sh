00626bd6127b9e8d731c58c6eb5b39de2cccc82b
PenguinL <diana1997525@gmail.com>
2020-10-10 14:29:01 +0800

    Create update_python.sh

---
+++ update_python.sh
@@ -0,0 +1,6 @@
+#https://medium.com/@rajputankit22/upgrade-python-2-7-to-3-6-and-3-7-in-ubuntu-97d2727bf911
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get install python3.6
+sudo apt-get install python3.7
+sudo apt-get install python3.8
+sudo apt-get install python3.9
