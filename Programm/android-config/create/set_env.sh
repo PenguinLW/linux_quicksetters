95d291273579be51e785bc4d0ecaf08ca44fbaf8
PenguinL <diana1997525@gmail.com>
2020-10-10 18:20:09 +0800

    Update set_env.sh

--- Programm/android-config/create/set_env.sh
+++ Programm/android-config/create/set_env.sh
@@ -2,9 +2,11 @@ sudo update-alternatives --install "/usr/bin/adb" "adb" "/home/penguinl/Programm
 sudo update-alternatives --set adb /home/penguinl/Programm/android-config/tools/platform-tools/adb
 cd .. && cd tools/platform-tools/build/scrcpy-1.16
 #sudo apt install meson
+sudo apt install ffmpeg libsdl2-2.0-0 -y
 sudo pip3 install meson
 sudo update-alternatives --install "/usr/bin/meson" "meson" "/usr/local/bin/meson" 1
 sudo update-alternatives --set meson /usr/local/bin/meson
+
 sudo apt install cmake -y
 
 meson build --buildtype release --strip -Db_lto=true  -Dprebuilt_server=../scrcpy-server-v1.16.jar
