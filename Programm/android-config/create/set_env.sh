5a2483153a961c1befc695ce543988ff23fa45a8
PenguinL <diana1997525@gmail.com>
2020-10-10 20:53:33 +0800

    Update set_env.sh

--- Programm/android-config/create/set_env.sh
+++ Programm/android-config/create/set_env.sh
@@ -22,3 +22,10 @@ sudo update-alternatives --install /usr/bin/scrcpy scrcpy /usr/local/bin/scrcpy 
 sudo update-alternatives --set scrcpy /usr/local/bin/scrcpy
 sudo update-alternatives --install /usr/bin/scrcpy-noconsole scrcpy-noconsole /usr/local/bin/scrcpy 1
 sudo update-alternatives --set scrcpy-noconsole /usr/local/bin/scrcpy
+
+
+
+sudo update-alternatives --install /usr/bin/adb_listofd adb_listofd /home/penguinl/Programm/android-config/src/adb_listofd.sh 1
+sudo update-alternatives --set adb_listofd /home/penguinl/Programm/android-config/src/adb_listofd.sh
+sudo update-alternatives --install /usr/bin/adb_restart adb_restart /home/penguinl/Programm/android-config/src/adb_restart.sh 1
+sudo update-alternatives --set adb_restart /home/penguinl/Programm/android-config/src/adb_restart.sh
