428b394162c8360596eccf1583a564422bb1eb91
PenguinL <diana1997525@gmail.com>
2020-10-10 20:55:55 +0800

    Update adb_restart.sh

--- Programm/android-config/src/adb_restart.sh
+++ Programm/android-config/src/adb_restart.sh
@@ -1,3 +1,5 @@
-cd .. && cd tools/platform-tools/
-./adb kill-server
-./adb start-server
+#cd .. && cd tools/platform-tools/
+#./adb kill-server
+#./adb start-server
+adb kill-server
+adb start-server
