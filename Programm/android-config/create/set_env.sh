c633032b7b5123bfa80e3e1fd20701a063472177
PenguinL <diana1997525@gmail.com>
2020-10-10 16:39:15 +0800

    Add files via upload

---
+++ Programm/android-config/create/set_env.sh
@@ -0,0 +1,2 @@
+sudo update-alternatives --install "/usr/bin/adb" "adb" "/home/penguinl/Programm/android-config/tools/platform-tools/adb" 1
+sudo update-alternatives --set adb /home/penguinl/Programm/android-config/tools/platform-tools/adb
