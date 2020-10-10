9dfd95f0e66a550994a666b129d4f811571c63cc
PenguinL <diana1997525@gmail.com>
2020-10-10 17:11:34 +0800

    Update set_env.sh

--- Programm/android-config/create/set_env.sh
+++ Programm/android-config/create/set_env.sh
@@ -1,2 +1,7 @@
 sudo update-alternatives --install "/usr/bin/adb" "adb" "/home/penguinl/Programm/android-config/tools/platform-tools/adb" 1
 sudo update-alternatives --set adb /home/penguinl/Programm/android-config/tools/platform-tools/adb
+cd .. && cd tools/platform-tools/build/scrcpy-1.16
+#sudo apt install meson
+sudo pip3 install meson
+sudo update-alternatives --install "/usr/bin/meson" "meson" "/usr/local/bin/meson" 1
+sudo update-alternatives --set meson /usr/local/bin/meson
