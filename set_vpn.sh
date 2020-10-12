5e7f01d87d2b857c033ba256d4a46ca402556cfc
PenguinL <diana1997525@gmail.com>
2020-10-12 21:19:30 +0800

    Update set_vpn.sh

--- set_vpn.sh
+++ set_vpn.sh
@@ -10,3 +10,7 @@ sudo apt install network-manager-l2tp network-manager-l2tp-gnome -y
 sudo apt-get install strongswan-plugin-openssl -y
 sudo service xl2tpd stop
 sudo update-rc.d xl2tpd disable
+
+
+#sudo apt install intltool libtool network-manager-dev libnm-util-dev libnm-glib-dev libnm-glib-vpn-dev libnm-gtk-dev libnm-dev libnma-dev ppp-dev strongswan libdbus-glib-1-dev libsecret-1-dev libgtk-3-dev libglib2.0-dev xl2tpd git libssl-dev libnss3-dev -y
+#
