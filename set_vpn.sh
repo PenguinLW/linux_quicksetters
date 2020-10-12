807922c97b4348e87a8ab4c424cf069ee687253b
PenguinL <diana1997525@gmail.com>
2020-10-12 23:10:48 +0800

    Update set_vpn.sh

--- set_vpn.sh
+++ set_vpn.sh
@@ -1,7 +1,4 @@
 #https://www.1denwin.info/2018/01/linux-l2tpipsec-mintubuntu.html
-#sudo apt-get install openvpn network-manager-openvpn-gnome
-sudo apt-get install network-manager-openvpn-gnome network-manager-vpnc-gnome -y
-sudo add-apt-repository ppa:nm-l2tp/network-manager-l2tp
 
 sudo apt-get update && sudo apt-get upgrade
 sudo apt update && sudo apt upgrade -y
