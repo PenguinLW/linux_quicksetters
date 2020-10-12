a5efc9421e319724ee578a56e8b684d71c01756b
PenguinL <diana1997525@gmail.com>
2020-10-12 23:16:40 +0800

    Update set_vpn.sh

--- set_vpn.sh
+++ set_vpn.sh
@@ -1,7 +1,1 @@
-#https://www.1denwin.info/2018/01/linux-l2tpipsec-mintubuntu.html
 
-sudo apt-get update && sudo apt-get upgrade
-sudo apt update && sudo apt upgrade -y
-
-#https://techlist.top/upgrade-openssl-version-1-1-0-ubuntu-server/
-#https://linuxmint.com.ru/viewtopic.php?t=647
