8a9db482411c0b5aa99ce3056382e242b7ed5a45
Kovalsky95 <diana1997525@gmail.com>
2020-10-13 22:06:26 +0800

    PenguinL

---
+++ 1024768/screen_off.py
@@ -0,0 +1,25 @@
+# https://habr.com/ru/post/118625/
+# sudo apt-get install python python-xlib
+# sudo apt-get install python python3-xlib
+# sudo apt install gnome-screensaver -y
+import time
+import subprocess
+from Xlib import X
+from Xlib.display import Display
+
+display = Display(':0')
+root = display.screen().root
+root.grab_pointer(True,
+                  X.ButtonPressMask | X.ButtonReleaseMask | X.PointerMotionMask,
+                  X.GrabModeAsync, X.GrabModeAsync, 0, 0, X.CurrentTime)
+root.grab_keyboard(True,
+                   X.GrabModeAsync, X.GrabModeAsync, X.CurrentTime)
+
+subprocess.call('xset dpms force off'.split())
+p = subprocess.Popen('gnome-screensaver-command --deactivate'.split())#gnome-screensaver-command --lock
+time.sleep(1)
+
+while True:
+    print(display.next_event())
+    p.terminate()
+    break
