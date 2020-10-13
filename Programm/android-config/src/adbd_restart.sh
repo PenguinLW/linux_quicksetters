6e54ffb62a4a51806cbfca5b030473add1a08b2a
PenguinL <diana1997525@gmail.com>
2020-10-13 15:08:05 +0800

    Add files via upload

---
+++ Programm/android-config/src/adbd_restart.sh
@@ -0,0 +1,6 @@
+adb -s KHGBB19B20603057 tcpip 5555
+adb -s 192.168.0.3:5555 tcpip 5555
+adb -s 0123456789ABCDEF shell su -c "setprop service.adb.tcp.port 5555 && stop adbd && start adbd"
+adb -s 0123456789ABCDEF tcpip 5555
+adb -s 192.168.0.4:5555 shell su -c "setprop service.adb.tcp.port 5555 && stop adbd && start adbd"
+adb -s 192.168.0.4:5555 tcpip 5555
