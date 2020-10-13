679451af7652d3614b22628e78eb75e46bba03e2
Kovalsky95 <diana1997525@gmail.com>
2020-10-13 16:33:07 +0800

    PenguinL

--- Programm/android-config/src/adbd_restart.sh
+++ Programm/android-config/src/adbd_restart.sh
@@ -3,4 +3,4 @@ adb -s 192.168.0.3:5555 tcpip 5555
 adb -s 0123456789ABCDEF shell su -c "setprop service.adb.tcp.port 5555 && stop adbd && start adbd"
 adb -s 0123456789ABCDEF tcpip 5555
 adb -s 192.168.0.4:5555 shell su -c "setprop service.adb.tcp.port 5555 && stop adbd && start adbd"
-adb -s 192.168.0.4:5555 tcpip 5555
+adb -s 192.168.0.4:5555 tcpip 5559
