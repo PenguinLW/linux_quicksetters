159bf21ade11cf1d44175427e702c632464adc74
Kovalsky95 <diana1997525@gmail.com>
2020-10-14 13:24:34 +0800

    PenguinL

--- update_python.sh
+++ update_python.sh
@@ -1,20 +1,22 @@
 #https://medium.com/@rajputankit22/upgrade-python-2-7-to-3-6-and-3-7-in-ubuntu-97d2727bf911
 sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get install python3.5
 sudo apt-get install python3.6
 sudo apt-get install python3.7
 sudo apt-get install python3.8
 sudo apt-get install python3.9
-which python3.5#default version
+which python3.5
 which python3.6
 which python3.7
 which python3.8
 which python3.9
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 3
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 4
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 5
+sudo apt-get install python3.5-dev -y
 sudo apt-get install python3.6-dev -y
 sudo apt-get install python3.7-dev -y
 sudo apt-get install python3.8-dev -y
 sudo apt-get install python3.9-dev -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 6
