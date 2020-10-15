7fd2563ebef897d3cf489615cec415d57f586ef4
Kovalsky95 <diana1997525@gmail.com>
2020-10-15 15:51:39 +0800

    PenguinL

---
+++ get_all.sh
@@ -0,0 +1,3 @@
+git clone -o linux_quicksetters https://github.com/Kovalsky95/linux_quicksetters.git
+rm -d -r -f Programm & mkdir Programm
+mv linux_quicksetters/* Programm/
--- update_python.sh
+++ update_python.sh
@@ -1,10 +1,10 @@
 #https://medium.com/@rajputankit22/upgrade-python-2-7-to-3-6-and-3-7-in-ubuntu-97d2727bf911
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get install python3.5
-sudo apt-get install python3.6
-sudo apt-get install python3.7
-sudo apt-get install python3.8
-sudo apt-get install python3.9
+sudo apt-get install python3.5 -y
+sudo apt-get install python3.6 -y
+sudo apt-get install python3.7 -y
+sudo apt-get install python3.8 -y
+sudo apt-get install python3.9 -y
 which python3.5
 which python3.6
 which python3.7
