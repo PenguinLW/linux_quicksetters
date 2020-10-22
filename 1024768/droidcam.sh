06d306f9da25277989d0343e5fa68049b76e4da8
Kovalsky95 <diana1997525@gmail.com>
2020-10-22 21:22:22 +0800

    PenguinL

--- 1024768/droidcam.sh
+++ 1024768/droidcam.sh
@@ -1,4 +1,3 @@
 #echo {your_password} | sudo -S ls
 echo 24121997di@n@GHI | sudo -S ls
-sudo droidcam
-pacmd load-module module-alsa-source device=hw:Loopback,1,0'
+sudo droidcam && gnome-terminal -e "pacmd load-module module-alsa-source device=hw:Loopback,1,0"
