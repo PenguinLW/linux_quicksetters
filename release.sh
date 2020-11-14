068397f0db219730aa08c75defd13f91c944f737
Kovalsky95 <diana1997525@gmail.com>
2020-11-14 21:29:00 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #git clone -o linux_quicksetters https://github.com/Kovalsky95/linux_quicksetters.git
+#git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=3600"
+#git config credential.helper store
+#git config --unset credential.helper
 git add .
 git commit -m PenguinL
 git push --set-upstream linux_quicksetters master
