2666a07071d7e2a716246394ad6e8cae12778f2c
PenguinL <diana1997525@gmail.com>
2021-04-24 10:21:19 +0800

    Update arm_env.sh

--- arm/arm_create/arm_env.sh
+++ arm/arm_create/arm_env.sh
@@ -8,3 +8,7 @@ termux-setup-storage
 apt install android-tools -y
 
 adb devices -l
+
+update-alternatives --install /usr/bin/arm_up arm_up /root/Programm/arm/arm_create/arm_up.sh 1
+
+update-alternatives --set arm_up /root/Programm/arm/arm_create/arm_up.sh
