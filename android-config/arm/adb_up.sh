b7e5f365e412f8bd7fe049511e8a7bd4e45467d3
PenguinL <diana1997525@gmail.com>
2021-04-24 10:02:11 +0800

    Arm's platforms adb set

---
+++ android-config/arm/adb_up.sh
@@ -0,0 +1,44 @@
+
+cd /root/Programm/android-config
+
+#cd /root/Programm/android-config/create
+
+#chmod +x *
+
+#./install.sh
+
+#/root/Programm/android-config/tools/tools/bin/sdkmanager --list
+
+#update-alternatives --install "/usr/bin/adb" "adb" "/root/Programm/android-config/tools/platform-tools/adb" 1
+
+#update-alternatives --set adb /root/Programm/android-config/tools/platform-tools/adb
+
+update-alternatives --install /usr/bin/adb_listofd adb_listofd /root/Programm/android-config/src/adb_listofd.sh 1
+
+update-alternatives --set adb_listofd /root/Programm/android-config/src/adb_listofd.sh
+
+update-alternatives --install /usr/bin/adb_restart adb_restart /root/Programm/android-config/src/adb_restart.sh 1
+
+update-alternatives --set adb_restart /root/Programm/android-config/src/adb_restart.sh
+
+update-alternatives --install /usr/bin/adbd_restart adbd_restart /root/Programm/android-config/src/adbd_restart.sh 1
+
+update-alternatives --set adbd_restart /root/Programm/android-config/src/adbd_restart.sh
+
+chmod +x /root/Programm/android-config/src/*.sh
+
+which adb
+
+which adb_listofd
+
+which adb_restart
+
+which adbd_restart
+
+#update-alternatives --remove adb /root/Programm/android-config/tools/platform-tools/adb
+
+#update-alternatives --remove adb_listofd /root/Programm/android-config/src/adb_listofd.sh
+
+#update-alternatives --remove adb_restart /root/Programm/android-config/src/adb_restart.sh
+
+#update-alternatives --remove adbd_restart /root/Programm/android-config/src/adbb_restart.sh
