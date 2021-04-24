4a3fd6c18f237403b54306416333c4b8e1f6b306
PenguinL <diana1997525@gmail.com>
2021-04-24 11:19:29 +0800

    Update show_env.sh

--- arm/show_env.sh
+++ arm/show_env.sh
@@ -1,3 +1,15 @@
+
+whoami
+
+uname -m #armv71
+
+java -version
+
+javac -version
+which java
+
+which javac
+
 which adb
 
 which adb_listofd
@@ -5,6 +17,7 @@ which adb_listofd
 which adb_restart
 
 which adbd_restart
+ls -al Programm
 
 ls -al /bin/
 
