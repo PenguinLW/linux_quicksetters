a7c32c782e6feac98fcfd15e1cc4b2c13135f6fb
PenguinL <diana1997525@gmail.com>
2021-04-24 10:32:37 +0800

    Update set_env.sh

--- arm/set_env.sh
+++ arm/set_env.sh
@@ -6,6 +6,6 @@ update-alternatives --install /usr/bin/get_all get_all /root/Programm/arm/get_al
 update-alternatives --set get_all /root/Programm/arm/get_all.sh
 
 
-update-alternatives --install /usr/bin/get_all get_all /root/Programm/arm/get_all.sh 1
+update-alternatives --install /usr/bin/update_upgrade_allp update_upgrade_allp /root/Programm/arm/update_upgrade_allp.sh 1
 
-update-alternatives --set get_all /root/Programm/arm/get_all.sh
+update-alternatives --set update_upgrade_allp /root/Programm/arm/update_upgrade_allp.sh
