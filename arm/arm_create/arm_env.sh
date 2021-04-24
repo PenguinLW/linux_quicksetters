37d6629ae1604382ff1abd879bc8b03856531993
PenguinL <diana1997525@gmail.com>
2021-04-24 10:21:38 +0800

    Update arm_env.sh

--- arm/arm_create/arm_env.sh
+++ arm/arm_create/arm_env.sh
@@ -9,6 +9,6 @@ apt install android-tools -y
 
 adb devices -l
 
-update-alternatives --install /usr/bin/arm_up arm_up /root/Programm/arm/arm_create/arm_up.sh 1
+#update-alternatives --install /usr/bin/arm_up arm_up /root/Programm/arm/arm_create/arm_up.sh 1
 
-update-alternatives --set arm_up /root/Programm/arm/arm_create/arm_up.sh
+#update-alternatives --set arm_up /root/Programm/arm/arm_create/arm_up.sh
