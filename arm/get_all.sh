b033f167151b20690673169042cb6e74fb1771e0
PenguinL <diana1997525@gmail.com>
2021-04-24 09:05:45 +0800

    Arm's platforms no need "rm" target dir

---
+++ arm/get_all.sh
@@ -0,0 +1,7 @@
+git clone -o linux_quicksetters https://github.com/Kovalsky95/linux_quicksetters.git
+
+#rm -d -r -f Programm & mkdir Programm
+
+mv linux_quicksetters/* Programm/
+
+rm -d -r -f linux_quicksetters 
