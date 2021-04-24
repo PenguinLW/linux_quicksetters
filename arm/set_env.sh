187477bb086f2bebc9c9f6be4071914fc4165bc1
PenguinL <diana1997525@gmail.com>
2021-04-24 11:47:06 +0800

    Update set_env.sh

--- arm/set_env.sh
+++ arm/set_env.sh
@@ -14,3 +14,7 @@ update-alternatives --set update_upgrade_allp /root/Programm/arm/update_upgrade_
 update-alternatives --install /usr/bin/show_env show_env /root/Programm/arm/show_env.sh 1
 
 update-alternatives --set show_env /root/Programm/arm/show_env.sh
+
+update-alternatives --install /usr/bin/adbd_restart adbd_restart /root/Programm/android-config/arm/src/adbd_restart.sh 1
+
+update-alternatives --set adbd_restart /root/Programm/android-config/arm/src/adbd_restart.sh
