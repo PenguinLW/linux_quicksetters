ef36e2f788e82c4eee414d2dad777dca4805a8b3
PenguinL <diana1997525@gmail.com>
2021-04-24 11:36:44 +0800

    Update adbd_restart.sh

--- android-config/arm/src/adbd_restart.sh
+++ android-config/arm/src/adbd_restart.sh
@@ -4,3 +4,25 @@
 
 
 #adb -s <your-device-id> shell setprop service.adb.tcp.port 5555
+
+
+
+#List of devices attached
+
+#192.168.43.62:5555     device product:DRA-LX5RU model:DRA_LX5 device:HWDRA-MG transport_id:2
+
+#emulator-5554          device product:DRA-LX5RU model:DRA_LX5 device:HWDRA-MG transport_id:1
+
+adb -s emulator-5554 shell setprop service.adb.tcp.port 5555
+
+adb -s emulator-5554 tcpip 5555
+
+adb -s 192.168.43.62 shell setprop service.adb.tcp.port 5555
+
+adb -s 192.168.43.62 tcpip 5555
+
+
+
+
+
+
