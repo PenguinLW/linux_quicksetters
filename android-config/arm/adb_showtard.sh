e0d6a15dd7cc6a893db997a8fb2a6444fd2da6d4
PenguinL <diana1997525@gmail.com>
2021-04-24 11:41:21 +0800

    Create adb_showtard.sh

---
+++ android-config/arm/adb_showtard.sh
@@ -0,0 +1,4 @@
+adb -s emulator-5554 shell ifconfig #192.168.43.62
+adb -s emulator-5554 shell pm list packages
+adb -s 192.168.43.62 shell ifconfig #192.168.43.62
+adb -s 192.168.43.62 shell pm list packages
