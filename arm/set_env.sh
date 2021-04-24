02855b53290e54a277832857f7cf074c61221d13
PenguinL <diana1997525@gmail.com>
2021-04-24 14:50:44 +0800

    Update set_env.sh

--- arm/set_env.sh
+++ arm/set_env.sh
@@ -1,31 +1,31 @@
 #in target os
-update-alternatives --install /usr/bin/get_all get_all Programm/arm/get_all.sh 1
+update-alternatives --install /usr/bin/get_all get_all /root/Programm/arm/get_all.sh 1
 
-update-alternatives --set get_all /Programm/arm/get_all.sh
-
-
-update-alternatives --install /usr/bin/update_upgrade_allp update_upgrade_allp Programm/arm/update_upgrade_allp.sh 1
-
-update-alternatives --set update_upgrade_allp Programm/arm/update_upgrade_allp.sh
+update-alternatives --set get_all /root/Programm/arm/get_all.sh
 
 
-update-alternatives --install /usr/bin/show_env show_env Programm/arm/show_env.sh 1
+update-alternatives --install /usr/bin/update_upgrade_allp update_upgrade_allp /root/Programm/arm/update_upgrade_allp.sh 1
 
-update-alternatives --set show_env Programm/arm/show_env.sh
-
-update-alternatives --install /usr/bin/adb_showtard adb_showtard Programm/android-config/arm/src/adb_showtard.sh 1
-
-update-alternatives --set adb_showtard Programm/android-config/arm/src/adb_showtard.sh
-
-update-alternatives --install /usr/bin/adbd_restart adbd_restart Programm/android-config/arm/src/adbd_restart.sh 1
-
-update-alternatives --set adbd_restart Programm/android-config/arm/src/adbd_restart.sh
-
-update-alternatives --install /usr/bin/y5_connect y5_connect Programm/1024768/arm/y5_connect.sh 1
-
-update-alternatives --set y5_connect Programm/1024768/arm/y5_connect.sh
+update-alternatives --set update_upgrade_allp /root/Programm/arm/update_upgrade_allp.sh
 
 
-update-alternatives --install /usr/bin/all_ejecution all_ejecution Programm/1024768/all_ejecution.sh 1
+update-alternatives --install /usr/bin/show_env show_env /root/Programm/arm/show_env.sh 1
 
-update-alternatives --set all_ejecution Programm/1024768/all_ejecution.sh
+update-alternatives --set show_env /root/Programm/arm/show_env.sh
+
+update-alternatives --install /usr/bin/adb_showtard adb_showtard /root/Programm/android-config/arm/src/adb_showtard.sh 1
+
+update-alternatives --set adb_showtard /root/Programm/android-config/arm/src/adb_showtard.sh
+
+update-alternatives --install /usr/bin/adbd_restart adbd_restart /root/Programm/android-config/arm/src/adbd_restart.sh 1
+
+update-alternatives --set adbd_restart /root/Programm/android-config/arm/src/adbd_restart.sh
+
+update-alternatives --install /usr/bin/y5_connect y5_connect /root/Programm/1024768/arm/y5_connect.sh 1
+
+update-alternatives --set y5_connect /root/Programm/1024768/arm/y5_connect.sh
+
+
+update-alternatives --install /usr/bin/all_ejecution all_ejecution /root/Programm/1024768/all_ejecution.sh 1
+
+update-alternatives --set all_ejecution /root/Programm/1024768/all_ejecution.sh
