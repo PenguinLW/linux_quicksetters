5de74e19af279baba0edbf220d5da671218b713d
PenguinL <diana1997525@gmail.com>
2021-05-21 00:08:18 +0800

    Update y5_monkey_sample.py

--- 1024768/y5_monkey_sample.py
+++ 1024768/y5_monkey_sample.py
@@ -3,4 +3,4 @@ swipe = ["adb -s 192.168.43.62 shell input swipe 250 50 0 50",
          "adb -s 192.168.43.62 shell input swipe 0 50 250 50"
         ] 
 
-os.system('{0:s} && {1:s}'.format(swipe[0], swipe[1])
+os.system('{0:s} && {1:s}'.format(swipe[0], swipe[1])) 
