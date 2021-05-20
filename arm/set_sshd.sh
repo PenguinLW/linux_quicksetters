5dee64b8e6b261d62c0663bf480626655fb0625b
PenguinL <diana1997525@gmail.com>
2021-05-20 17:39:14 +0800

    Update set_sshd.sh

--- arm/set_sshd.sh
+++ arm/set_sshd.sh
@@ -2,5 +2,5 @@
 apt install openssh
 pkill sshd
 passwd
-sshd #. /storage/648D-16E9/sshd.sh #. /Programm/1024768/arm/sshd.sh
-ssh -p 8022 192.168.43.62
+sshd #. /Programm/1024768/arm/sshd.sh
+#ssh -p 8022 192.168.43.62
