5eb5918213af22819cc8159c36eab239411b0063
PenguinL <diana1997525@gmail.com>
2021-05-20 22:58:14 +0800

    Create which_pip_of_python_3.sh

---
+++ arm/which_pip_of_python_3.sh
@@ -0,0 +1,24 @@
+#apt install python-pip -y
+
+which python
+
+python -V
+
+which pip
+
+pip -V
+
+apt install python3-pip -y
+
+which python3
+
+python3 -V
+
+which pip3
+
+pip3 -V
+
+#https://github.com/pypa/pip/issues/5599
+
+pip3 install --upgrade pip
+
