a0ef11b7524dd54c4efb2fbfadb91091787b5306
PenguinL <diana1997525@gmail.com>
2021-05-20 22:53:29 +0800

    Create update_python.sh

---
+++ arm/update_python.sh
@@ -0,0 +1,23 @@
+#https://medium.com/@rajputankit22/upgrade-python-2-7-to-3-6-and-3-7-in-ubuntu-97d2727bf911
+#add-apt-repository ppa:deadsnakes/ppa
+#apt-get install python3.5 -y
+#apt-get install python3.6 -y
+#apt-get install python3.7 -y
+apt-get install python3.8 -y
+#apt-get install python3.9 -y
+which python3.5
+which python3.6
+which python3.7
+which python3.8
+which python3.9
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 3
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 4
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 5
+#apt-get install python3.5-dev -y
+#apt-get install python3.6-dev -y
+#apt-get install python3.7-dev -y
+apt-get install python3.8-dev -y
+#apt-get install python3.9-dev -y
+
