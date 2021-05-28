ee57030e2c785d564e97a32f86c6b77ff2519aa0
Kovalsky95 <diana1997525@gmail.com>
2021-05-28 19:01:06 +0800

    PenguinL

--- android-config/create/set_env.sh
+++ android-config/create/set_env.sh
@@ -1,22 +1,6 @@
 #https://www.linuxuprising.com/2019/03/control-android-devices-from-your.html
 sudo update-alternatives --install "/usr/bin/adb" "adb" "/home/penguinl/Programm/android-config/tools/platform-tools/adb" 1
 sudo update-alternatives --set adb /home/penguinl/Programm/android-config/tools/platform-tools/adb
-cd .. && cd tools/platform-tools/build/scrcpy-1.16
-#sudo apt install meson
-#https://github.com/Genymobile/scrcpy/blob/master/BUILD.md
-sudo apt install ffmpeg libsdl2-2.0-0 -y
-sudo apt install gcc git pkg-config meson ninja-build \
-                 libavcodec-dev libavformat-dev libavutil-dev \
-                 libsdl2-dev -y
-sudo pip3 install meson
-sudo update-alternatives --install "/usr/bin/meson" "meson" "/usr/local/bin/meson" 1
-sudo update-alternatives --set meson /usr/local/bin/meson
-
-sudo apt install cmake -y
-
-meson build --buildtype release --strip -Db_lto=true  -Dprebuilt_server=../../scrcpy-server-v1.16.jar
-cd build && sudo ninja
-sudo ninja install
 
 sudo update-alternatives --install /usr/bin/scrcpy scrcpy /usr/local/bin/scrcpy 1
 sudo update-alternatives --set scrcpy /usr/local/bin/scrcpy
---
+++ android-config/create/set_scrcpy.sh
@@ -0,0 +1,16 @@
+cd .. && cd tools/platform-tools/build/scrcpy-1.17
+#sudo apt install meson
+#https://github.com/Genymobile/scrcpy/blob/master/BUILD.md
+sudo apt install ffmpeg libsdl2-2.0-0 -y
+sudo apt install gcc git pkg-config meson ninja-build \
+                 libavcodec-dev libavformat-dev libavutil-dev \
+                 libsdl2-dev -y
+sudo pip3 install meson
+sudo update-alternatives --install "/usr/bin/meson" "meson" "/usr/local/bin/meson" 1
+sudo update-alternatives --set meson /usr/local/bin/meson
+
+sudo apt install cmake -y
+
+meson build --buildtype release --strip -Db_lto=true  -Dprebuilt_server=../../scrcpy-server-v1.16.jar
+cd build && sudo ninja
+sudo ninja install
