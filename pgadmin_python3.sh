9074fc9add5b147c9205d1a6c2a4aa4ac0a8dc11
Kovalsky95 <diana1997525@gmail.com>
2021-06-08 17:14:56 +0800

    PenguinL

--- pgadmin_python3.sh
+++ pgadmin_python3.sh
@@ -1,16 +1,23 @@
-#sudo apt-get install python3-venv
+#sudo apt-get install python3-venv -y
 #sudo python3 -m venv pgadmin
 #source pgadmin/bin/activate
-#sudo pip3 install setuptools
-sudo apt-get install python3.5-dev -y
+
+#pip3 install --upgrade pip
+#pip3 install setuptools
+pip3 install setuptools_rust
+#sudo apt-get install python3.7-dev -y
+#pip3 install wheel
+
 #pip3 install https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.23/pip/pgadmin4-4.23-py3-none-any.whl --ignore-installed
 #sudo pip3 install https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.26/pip/pgadmin4-4.26-py3-none-any.whl --ignore-installed
-sudo pip3 install https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.27/pip/pgadmin4-4.27-py3-none-any.whl --ignore-installed
+pip3 install https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.27/pip/pgadmin4-4.27-py3-none-any.whl --ignore-installed
 #python3 ~/pgadmin4/lib/python3.8/site-packages/pgadmin4/pgAdmin4.py
+
+#for update command
+sudo pip3 install --upgrade pgadmin4 #https://files.pythonhosted.org/packages/b5/c6/a78f02964ed956c6a3846e47a6519f522b3c902f745410d281f9215ae0ea/pgadmin4-5.3-py3-none-any.whl
+
 which pgadmin4
 #/usr/local/bin/pgadmin4
 sudo pgadmin4
 #http://127.0.0.1:5050
-#for update command
-sudo pip3 install --upgrade pgadmin4
 #deactivate
--- update_python.sh
+++ update_python.sh
@@ -26,3 +26,10 @@ sudo apt-get upgrade python3.6 -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
 #set pip?pip3, make scrcpy ..
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 3
+
+
+sudo apt-get install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
+sudo apt-get install python3-pip python3.7-dev
+sudo apt-get install python3.7
+sudo apt-get install python3-dev python3-setuptools python3-wheel -y
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 5
