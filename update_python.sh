b4e2ef01b6cfd9253c62b9349e6c2473bb2d6d23
Kovalsky95 <diana1997525@gmail.com>
2021-06-08 17:54:56 +0800

    PenguinL

--- update_python.sh
+++ update_python.sh
@@ -33,3 +33,6 @@ sudo apt-get install python3-pip python3.7-dev
 sudo apt-get install python3.7
 sudo apt-get install python3-dev python3-setuptools python3-wheel -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 5
+
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 6
