a078127b2b04eb852cea0f1bc2906d51e74e5f73
Kovalsky95 <diana1997525@gmail.com>
2021-06-08 18:17:13 +0800

    PenguinL

--- update_python.sh
+++ update_python.sh
@@ -36,3 +36,5 @@ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 5
 
 
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 6
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 7
+#sudo apt-get install cinnamon cinnamon-common --reinstall
