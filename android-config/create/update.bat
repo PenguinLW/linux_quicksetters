d39d72c0e035c17e8691f17e4b27b73feb5f9ca6
PenguinL <diana1997525@gmail.com>
2021-06-09 18:32:11 +0800

    Create update.bat

--- android-config/create/update.bat
+++ android-config/create/update.bat
@@ -3,5 +3,7 @@ rem adb sideload c:\Programm\android-config\src\SuperSU+PRO+v2.82SR5.zip
 rem adb -s 192.168.0.4:5555 sideload c:\Programm\android-config\resources\UPDATE-SuperSU-v2.79-20161211114519.zip
 rem adb -s 0123456789ABCDEF sideload c:\Programm\android-config\resources\UPDATE-SuperSU-v2.79-20161211114519.zip
 rem fastboot update c:\Programm\android-config\resources\SuperSU+PRO+v2.82SR5.zip
+
+rem in TWRP touch "Advanced + ADB Sideload"
 rem adb -s 0123456789ABCDEF sideload G:\HomTom_mod_v1.7.zip
 pause
