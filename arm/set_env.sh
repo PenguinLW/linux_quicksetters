7eaa2df4e1a7b77b2d16c7a7c9b1b5b998ae7fb5
Kovalsky95 <diana1997525@gmail.com>
2021-06-27 22:40:36 +0800

    PenguinL

--- arm/set_env.sh
+++ arm/set_env.sh
@@ -33,8 +33,8 @@ update-alternatives --set hw_ssh /root/Programm/1024768/hw_ssh.sh
 update-alternatives --install /usr/bin/y5_unlock_screen y5_unlock_screen /root/Programm/1024768/y5_unlock_screen.sh 1
 update-alternatives --set y5_unlock_screen /root/Programm/1024768/y5_unlock_screen.sh
 
-update-alternatives --install /usr/bin/hw_connect hw_connect /root/Programm/1024768/hw_connect.sh 1
-update-alternatives --set hw_connect /root/Programm/1024768/huawei_connect.sh
+update-alternatives --install /usr/bin/hw_connect huawei_connect /root/Programm/1024768/huawei_connect.sh 1
+update-alternatives --set huawei_connect /root/Programm/1024768/huawei_connect.sh
 
 chmod +x /root/Programm/arm/*.sh
 chmod +x /root/Programm/1024768/*.sh
