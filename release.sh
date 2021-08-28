1c12b346ed21500877536204e9b9d6d6f69502b7
Kovalsky95 <diana1997525@gmail.com>
2021-08-28 16:31:17 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #in venv
-. /home/penguinl/Documentos/PycharmProjects/linux_quicksetters/venv/bin/activate
+. /venv/bin/activate
 #pip install -r req.txt
 pip freeze > req.txt
 #git clone -o linux_quicksetters https://github.com/Kovalsky95/linux_quicksetters.git
