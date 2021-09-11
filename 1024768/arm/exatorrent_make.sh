1a7835a4e4307be8dfd18e6ee268e226dd6ef32f
PenguinL <diana1997525@gmail.com>
2021-09-11 17:41:19 +0800

    Update exatorrent_make.sh

--- 1024768/arm/exatorrent_make.sh
+++ 1024768/arm/exatorrent_make.sh
@@ -1,6 +1,10 @@
 #in venv
 git clone https://github.com/varbhat/exatorrent.git
-pip3 install nodeenv
-nodeenv nenv
-. nenv/bin/activate
+
+#pip3 install nodeenv
+#nodeenv nenv
+#. nenv/bin/activate
+
+wget https://nodejs.org/dist/v16.9.1/node-v16.9.1-linux-armv7l.tar.xz --no-check-certificate #wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-armv7l.tar.xz --no-check-certificate
+tar -xpJf node-v16.9.1-linux-armv7l.tar.xz && rm -fr node-v16.9.1-linux-armv7l.tar.xz
 make web && make app
