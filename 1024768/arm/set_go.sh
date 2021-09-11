6909e8a2bc0399eb014f1c2b381037102a68b78f
PenguinL <diana1997525@gmail.com>
2021-09-11 21:11:05 +0800

    Update set_go.sh

--- 1024768/arm/set_go.sh
+++ 1024768/arm/set_go.sh
@@ -1,3 +1,4 @@
 #in venv
-#https://golang.org/dl/
+#https://golang.org/dl/ #https://www.tecmint.com/install-go-in-ubuntu/
 wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz --no-check-certificate
+tar -xvzf go1.17.1.linux-amd64.tar.gz && rm -fr go1.17.1.linux-amd64.tar.gz
