58c1b1bf525f0b3d58896fa53d7595727ea608d8
PenguinL <diana1997525@gmail.com>
2021-09-12 00:46:55 +0800

    Create exatorrent.sh

---
+++ 1024768/arm/exatorrent.sh
@@ -0,0 +1,4 @@
+#https://github.com/varbhat/exatorrent/releases
+wget https://github.com/varbhat/exatorrent/releases/download/v0.0.2/exatorrent-linux-arm64 --no-check-certificate
+chmod u+x ./exatorrent-linux-arm64
+./exatorrent-linux-arm64
