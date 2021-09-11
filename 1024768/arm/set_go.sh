2bb917fd986b5dab118b5f75a89a0572935003f8
PenguinL <diana1997525@gmail.com>
2021-09-11 21:44:25 +0800

    Update set_go.sh

--- 1024768/arm/set_go.sh
+++ 1024768/arm/set_go.sh
@@ -1,9 +1,12 @@
 #in venv
-#https://golang.org/dl/ #https://www.tecmint.com/install-go-in-ubuntu/
-wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz --no-check-certificate
-tar -xvzf go1.17.1.linux-amd64.tar.gz && rm -fr go1.17.1.linux-amd64.tar.gz
+#https://golang.org/doc/install #https://golang.org/dl/ #https://www.tecmint.com/install-go-in-ubuntu/ #https://github.com/golang/go/wiki/GoArm
+
+wget https://golang.org/dl/go1.17.1.linux-386.tar.gz --no-check-certificate #https://golang.org/dl/go1.17.1.linux-amd64.tar.gz #https://golang.org/dl/go1.17.1.linux-arm64.tar.gz #https://golang.org/dl/go1.17.1.linux-armv6l.tar.gz
+tar -xvzf go1.17.1.linux-386.tar.gz && rm -fr go1.17.1.linux-386.tar.gz #tar -xvzf go1.17.1.linux-amd64.tar.gz && rm -fr go1.17.1.linux-amd64.tar.gz
 
 update-alternatives --install /usr/bin/go go /root/go/bin/go 1
 update-alternatives --set go /root/go/bin/go
 update-alternatives --install /usr/bin/gofmt gofmt /root/go/bin/gofmt 1
 update-alternatives --set gofmt /root/go/bin/gofmt
+
+go version
