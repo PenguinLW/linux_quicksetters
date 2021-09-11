e163e01cacd6011437552daf6755cb2bdf6cd42d
PenguinL <diana1997525@gmail.com>
2021-09-11 22:45:37 +0800

    Update exatorrent_make_node_npm.sh

--- 1024768/arm/exatorrent_make_node_npm.sh
+++ 1024768/arm/exatorrent_make_node_npm.sh
@@ -13,3 +13,6 @@ make web #make web && make app
 
 #npm install
 #npm run-script build
+
+make app #make web && make app
+#env CGO_ENABLED=1 go build -trimpath -buildmode=pie -ldflags '-extldflags "-static -s -w"' -o  build/exatorrent exatorrent.go
