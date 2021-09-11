4674e0a21b7dea89cddd0c4f79ae600f057b4d33
PenguinL <diana1997525@gmail.com>
2021-09-11 22:42:21 +0800

    Update exatorrent_make_node_npm.sh

--- 1024768/arm/exatorrent_make_node_npm.sh
+++ 1024768/arm/exatorrent_make_node_npm.sh
@@ -4,8 +4,11 @@ git clone https://github.com/varbhat/exatorrent.git
 cd exatorrent/
 make web #make web && make app
 cd internal/web/
+
 npm fund
 #in package-lock.json "lockfileVersion": 1 or the code "hasInstallScript": true
 npm config get ignore-scripts
 npm config set ignore-scripts true
+
+npm install
 npm run-script build
