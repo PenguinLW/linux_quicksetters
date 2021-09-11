56446ace77d1a342a3642e761933edb4c14a0564
PenguinL <diana1997525@gmail.com>
2021-09-11 21:55:23 +0800

    Update set_go.sh

--- 1024768/arm/set_go.sh
+++ 1024768/arm/set_go.sh
@@ -2,7 +2,7 @@
 #https://golang.org/doc/install #https://golang.org/dl/ #https://www.tecmint.com/install-go-in-ubuntu/ #https://github.com/golang/go/wiki/GoArm
 #https://www.e-tinkers.com/2019/06/better-way-to-install-golang-go-on-raspberry-pi/
 
-wget https://golang.org/dl/go1.17.1.linux-armv6l.tar.gz --no-check-certificate #https://golang.org/dl/go1.17.1.linux-amd64.tar.gz #https://golang.org/dl/go1.17.1.linux-386.tar.gz
+wget https://golang.org/dl/go1.17.1.linux-armv6l.tar.gz --no-check-certificate #https://golang.org/dl/go1.17.1.linux-386.tar.gz #https://golang.org/dl/go1.17.1.linux-amd64.tar.gz #https://golang.org/dl/go1.17.1.linux-arm64.tar.gz
 tar -xvzf go1.17.1.linux-armv6l.tar.gz && rm -fr go1.17.1.linux-armv6l.tar.gz
 
 update-alternatives --install /usr/bin/go go /root/go/bin/go 1
