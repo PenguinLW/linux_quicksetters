9b1dbad0e6307cc01e46dc8951e80b1f395b3bfe
PenguinL <diana1997525@gmail.com>
2021-09-11 20:39:18 +0800

    Update exatorrent_make.sh

--- 1024768/arm/exatorrent_make.sh
+++ 1024768/arm/exatorrent_make.sh
@@ -4,3 +4,6 @@ git clone https://github.com/varbhat/exatorrent.git
 cd exatorrent/
 make web && make app
 npm fund
+#in package-lock.json "lockfileVersion": 1 or the code "hasInstallScript": true
+npm config set ignore-scripts true
+npm run-script build
