ad8873c61ed94faff8d0d41fbf4787e206bfcf1d
PenguinL <diana1997525@gmail.com>
2021-09-12 01:20:17 +0800

    Update set_node_npm_npx.sh

--- 1024768/arm/set_node_npm_npx.sh
+++ 1024768/arm/set_node_npm_npx.sh
@@ -27,6 +27,10 @@ update-alternatives --set npx /root/node-v10.16.1-linux-armv7l/bin/npx
 #npm install -g npm
 ##npm fund
 
+apt install libssl1.0-dev
+apt install nodejs-dev
+apt install node-gyp -y
+
 node -v
 npm -v
 npx -v
