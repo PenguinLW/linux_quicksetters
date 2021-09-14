d07bd7ff4d4a5235168f130b325ce19c56b68578
PenguinL <diana1997525@gmail.com>
2021-09-14 21:14:57 +0800

    Update install.sh

--- android-config/create/install.sh
+++ android-config/create/install.sh
@@ -2,4 +2,9 @@
 cd .. && cd tools/tools/bin/
 ./sdkmanager --list
 ./sdkmanager "platform-tools" "platforms;android-23"
+./sdkmanager "platform-tools" "platforms;android-25"
 ./sdkmanager "platform-tools" "platforms;android-26"
+#./sdkmanager "platform-tools" "platforms;android-27"
+#./sdkmanager "platform-tools" "platforms;android-28"
+#./sdkmanager "platform-tools" "platforms;android-29"
+#./sdkmanager "platform-tools" "platforms;android-30"
