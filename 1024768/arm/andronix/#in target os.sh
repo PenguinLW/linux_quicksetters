e060635970e701322e92526b5001d1bfdae88ab2
PenguinL <diana1997525@gmail.com>
2021-11-09 12:00:11 +0800

    Update #in target os.sh

--- 1024768/arm/andronix/#in target os.sh
+++ 1024768/arm/andronix/#in target os.sh
@@ -5,6 +5,7 @@ apt install git -y
 apt install net-tools -y
 apt install usbutils -y
 apt install libudev-dev libusb-1.0-0-dev -y
+apt install libudev-dev libusb-dev usbutils -y
 
 vncserver-start
 vncserver -list
