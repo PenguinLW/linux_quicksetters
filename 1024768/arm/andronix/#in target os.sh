f7b4389dd026b3d754d7a110b6e9ae5291b99d94
PenguinL <diana1997525@gmail.com>
2021-11-09 12:17:48 +0800

    Update #in target os.sh

--- 1024768/arm/andronix/#in target os.sh
+++ 1024768/arm/andronix/#in target os.sh
@@ -4,7 +4,7 @@ apt install curl -y
 apt install git -y
 apt install net-tools -y
 apt install usbutils libudev-dev libusb-1.0-0-dev libusb-dev -y
-apt install v4l-utils -y && sudo v4l-utils -y
+apt install v4l-utils -y && sudo v4l-ctl --list-devices
 
 vncserver-start
 vncserver -list
