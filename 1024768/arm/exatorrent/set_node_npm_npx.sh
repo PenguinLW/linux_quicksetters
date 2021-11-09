b61855188516d328bb61813bdb46c626af26031c
PenguinL <diana1997525@gmail.com>
2021-11-09 16:52:11 +0800

    Update set_node_npm_npx.sh

--- 1024768/arm/exatorrent/set_node_npm_npx.sh
+++ 1024768/arm/exatorrent/set_node_npm_npx.sh
@@ -5,6 +5,9 @@
 #nodeenv nenv
 #. nenv/bin/activate
 
+wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-armv7l.tar.xz --no-check-certificate
+tar -xpJf node-v10.16.3-linux-armv7l.tar.xz && rm -fr node-v10.16.3-linux-armv7l.tar.xz
+
 wget https://nodejs.org/dist/v10.16.1/node-v10.16.1-linux-armv7l.tar.xz --no-check-certificate
 tar -xpJf node-v10.16.1-linux-armv7l.tar.xz && rm -fr node-v10.16.1-linux-armv7l.tar.xz
 update-alternatives --install /usr/bin/node node /root/node-v10.16.1-linux-armv7l/bin/node 1
@@ -15,8 +18,8 @@ update-alternatives --install /usr/bin/npx npx /root/node-v10.16.1-linux-armv7l/
 update-alternatives --set npx /root/node-v10.16.1-linux-armv7l/bin/npx
 
 ##https://nodejs.org/en/download/current/
-#wget https://nodejs.org/dist/v16.9.1/node-v16.9.1-linux-armv7l.tar.xz --no-check-certificate #wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-armv7l.tar.xz --no-check-certificate
-#tar -xpJf node-v16.9.1-linux-armv7l.tar.xz && rm -fr node-v16.9.1-linux-armv7l.tar.xz
+wget https://nodejs.org/dist/v16.9.1/node-v16.9.1-linux-armv7l.tar.xz --no-check-certificate 
+tar -xpJf node-v16.9.1-linux-armv7l.tar.xz && rm -fr node-v16.9.1-linux-armv7l.tar.xz
 #update-alternatives --install /usr/bin/node node /root/node-v16.9.1-linux-armv7l/bin/node 1
 #update-alternatives --set node /root/node-v16.9.1-linux-armv7l/bin/node
 #update-alternatives --install /usr/bin/npm npm /root/node-v16.9.1-linux-armv7l/bin/npm 1
