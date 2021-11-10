9c4fe5e8b1defc8ca492041b813a5c58c0ee1093
PenguinL <diana1997525@gmail.com>
2021-11-10 19:26:39 +0800

    Create opencv_python.sh

---
+++ 1024768/arm/andronix/opencv_python.sh
@@ -0,0 +1,12 @@
+#in target os
+#apt install usbutils libudev-dev libusb-1.0-0-dev libusb-dev -y
+#apt install v4l-utils -y && sudo v4l-ctl --list-devices
+#apt install ffmpeg -y #ffplay /dev/video0
+#apt install python3.9 -y
+#apt install python3.9-dev -y
+#apt install python3.9-venv -y
+
+#pip install opencv-python
+#pip install opencv-contrib-python
+#pip install opencv-python-headless
+#pip install opencv-contrib-python-headless
