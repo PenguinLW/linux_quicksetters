7eb46b104a35edb15f5491152217e4f205ae0a27
PenguinL <diana1997525@gmail.com>
2021-11-10 19:31:46 +0800

    Update opencv_python.sh

--- 1024768/arm/andronix/opencv_python.sh
+++ 1024768/arm/andronix/opencv_python.sh
@@ -1,4 +1,7 @@
 #in target os
+##https://pypi.org/project/opencv-python-armv7l/
+
+
 #apt install usbutils libudev-dev libusb-1.0-0-dev libusb-dev -y
 #apt install v4l-utils -y && sudo v4l-ctl --list-devices
 #apt install ffmpeg -y #ffplay /dev/video0
