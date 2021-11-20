8f7eee56ef59b99ffbabf0f2c4965fec62c1b62d
PenguinL <diana1997525@gmail.com>
2021-11-21 06:48:16 +0800

    Update set_env.sh

--- arm/set_env.sh
+++ arm/set_env.sh
@@ -24,8 +24,17 @@ update-alternatives --set y5_connect /root/Programm/1024768/y5_connect.sh
 update-alternatives --install /usr/bin/ixion_connect ixion_connect /root/Programm/1024768/ixion_connect.sh 1
 update-alternatives --set ixion_connect /root/Programm/1024768/ixion_connect.sh
 
+update-alternatives --install /usr/bin/xm_connect xm_connect /root/Programm/1024768/xm_connect.sh 1
+update-alternatives --set xm_connect /root/Programm/1024768/xm_connect.sh
+
 update-alternatives --install /usr/bin/all_ejecution all_ejecution /root/Programm/1024768/all_ejecution.sh 1
 update-alternatives --set all_ejecution /root/Programm/1024768/all_ejecution.sh
+
+update-alternatives --install /usr/bin/sh_reboot sh_reboot /root/Programm/1024768/sh_reboot.sh 1
+update-alternatives --set sh_reboot /root/Programm/1024768/sh_reboot.sh
+
+update-alternatives --install /usr/bin/screen_off screen_off /root/Programm/1024768/screen_off.sh 1
+update-alternatives --set screen_off /root/Programm/1024768/screen_off.sh
 
 update-alternatives --install /usr/bin/y5_ssh y5_ssh /root/Programm/1024768/y5_ssh.sh 1
 update-alternatives --set y5_ssh /root/Programm/1024768/y5_ssh.sh
@@ -36,15 +45,24 @@ update-alternatives --set hw_ssh /root/Programm/1024768/hw_ssh.sh
 update-alternatives --install /usr/bin/ixion_ssh ixion_ssh /root/Programm/1024768/ixion_ssh.sh 1
 update-alternatives --set ixion_ssh /root/Programm/1024768/ixion_ssh.sh
 
+update-alternatives --install /usr/bin/xm_ssh xm_ssh /root/Programm/1024768/xm_ssh.sh 1
+update-alternatives --set xm_ssh /root/Programm/1024768/xm_ssh.sh
+
 update-alternatives --install /usr/bin/y5_unlock_screen y5_unlock_screen /root/Programm/1024768/y5_unlock_screen.sh 1
 update-alternatives --set y5_unlock_screen /root/Programm/1024768/y5_unlock_screen.sh
+
+update-alternatives --install /usr/bin/xm_unlock_screen xm_unlock_screen /root/Programm/1024768/xm_unlock_screen.sh 1
+update-alternatives --set xm_unlock_screen /root/Programm/1024768/xm_unlock_screen.sh
 
 update-alternatives --install /usr/bin/huawei_connect huawei_connect /root/Programm/1024768/huawei_connect.sh 1
 update-alternatives --set huawei_connect /root/Programm/1024768/huawei_connect.sh
 
-chmod +x /root/Programm/arm/*.sh
-chmod +x /root/Programm/1024768/*.sh
-chmod +x /root/Programm/1024768/*.py
-chmod +x /root/Programm/1024768/arm/*.sh
-chmod +x /root/Programm/android-config/src/*.sh
-chmod +x /root/Programm/android-config/arm/src/*.sh
+update-alternatives --install /usr/bin/huawei_connect xm_connect /root/Programm/1024768/xm_connect.sh 1
+update-alternatives --set xm_connect /root/Programm/1024768/xm_connect.sh
+
+chmod u+x /root/Programm/arm/*.sh
+chmod u+x /root/Programm/1024768/*.sh
+chmod u+x /root/Programm/1024768/*.py
+chmod u+x /root/Programm/1024768/arm/*.sh
+chmod u+x /root/Programm/android-config/src/*.sh
+chmod u+x /root/Programm/android-config/arm/src/*.sh
