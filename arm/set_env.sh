697ef49f74ce2d0ea456beb30f2fcd37a710e149
PenguinL <diana1997525@gmail.com>
2021-11-21 07:43:33 +0800

    Update set_env.sh

--- arm/set_env.sh
+++ arm/set_env.sh
@@ -35,7 +35,7 @@ update-alternatives --set ixion_show /root/Programm/1024768/ixion_show.sh
 update-alternatives --install /usr/bin/huawei_show huawei_show /root/Programm/1024768/huawei_show.sh 1
 update-alternatives --set huawei_show /root/Programm/1024768/huawei_show.sh
 update-alternatives --install /usr/bin/y5_show y5_show /root/Programm/1024768/y5_show.sh 1
-update-alternatives --set y5_show /home/penguinl/Programm/1024768/y5_show.sh
+update-alternatives --set y5_show /root/Programm/1024768/y5_show.sh
 update-alternatives --install /usr/bin/xm_show xm_show /root/Programm/1024768/xm_show.sh 1
 update-alternatives --set xm_show /root/Programm/1024768/xm_show.sh
 
