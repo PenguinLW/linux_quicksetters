caf9920d29e7c1dd92342b46fdf901587aca5d37
PenguinL <diana1997525@gmail.com>
2021-11-21 06:12:44 +0800

    Update xm_unlock_screen.sh

--- 1024768/xm_unlock_screen.sh
+++ 1024768/xm_unlock_screen.sh
@@ -1,1 +1,19 @@
+#chmod +x Programm/1024768/y5_unlock_screen.sh && . Programm/1024768/y5_unlock_screen.sh
+adb-s 192.168.43.167 shell input keyevent 26
 
+#adb -s 192.168.43.167 shell input keyevent 66
+#adb -s 192.168.43.167 shell input keyevent 66
+
+adb -s 192.168.43.167 shell input tap 360 1425
+adb -s 192.168.43.167 shell input tap 360 1425
+adb -s 192.168.43.167 shell input tap 360 1425
+
+adb -s 192.168.43.167 shell input text 0_penguinl
+adb -s 192.168.43.167 shell input keyevent 66
+
+#adb shell input keyevent 3
+#adb shell input keyevent --longpress 1
+#adb shell input touchscreen tap 320 1182
+
+adb -s 192.168.43.167:5555 shell input tap 360 1425
+adb -s 192.168.43.167:5555 shell input tap 320 1430
