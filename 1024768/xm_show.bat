6b6424d0a9292c672e1baeb23f5b8efab3345702
PenguinL <diana1997525@gmail.com>
2021-11-21 06:30:30 +0800

    Create xm_show.bat

---
+++ 1024768/xm_show.bat
@@ -0,0 +1,5 @@
+@echo off
+rem #adb -s <your-device-id> tcpip 5555
+rem #adb -s 192.168.43.167:5555 tcpip 5555
+rem #scrcpy -s <your-device-id>
+start scrcpy-noconsole --always-on-top -w -t -s 192.168.43.167:5555
