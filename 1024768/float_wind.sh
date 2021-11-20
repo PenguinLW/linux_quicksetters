5c2eb408ece0f7bd31010bc464571896501e546e
PenguinL <diana1997525@gmail.com>
2021-11-21 05:59:16 +0800

    Update and rename hw_float_wind.sh to float_wind.sh

---
+++ 1024768/float_wind.sh
@@ -0,0 +1,2 @@
+#adb -s <your-device-id> shell settings put global enable_freeform_support 1
+#adb -s <your-device-id>:5555 shell settings put global enable_freeform_support 1
--- 1024768/hw_float_wind.sh
+++
@@ -1,2 +0,0 @@
-adb -s KHGBB19B20603057 shell settings put global enable_freeform_support 1
-adb -s 192.168.43.1 shell settings put global enable_freeform_support 1
