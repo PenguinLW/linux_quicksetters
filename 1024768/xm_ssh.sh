c5a559e488b83585eaa525dfa489af67c950162e
PenguinL <diana1997525@gmail.com>
2021-11-21 06:37:55 +0800

    Update xm_ssh.sh

--- 1024768/xm_ssh.sh
+++ 1024768/xm_ssh.sh
@@ -1,3 +1,3 @@
-#chmod +x Programm/1024768/xm_ssh.sh && . Programm/1024768/xm_ssh.sh
+#chmod u+x Programm/1024768/xm_ssh.sh && . Programm/1024768/xm_ssh.sh
 
-ssh -p 8022 192.168.43.167 
+ssh -p 8022 192.168.43.167
