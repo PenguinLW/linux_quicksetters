b3ffbe8b9d385905717bc33f6e493c8798e6ca5b
PenguinL <diana1997525@gmail.com>
2021-11-21 06:37:00 +0800

    Update y5_ssh.sh

--- 1024768/y5_ssh.sh
+++ 1024768/y5_ssh.sh
@@ -1,3 +1,3 @@
-#chmod +x Programm/1024768/y5_ssh.sh && . Programm/1024768/y5_ssh.sh
+#chmod u+x Programm/1024768/y5_ssh.sh && . Programm/1024768/y5_ssh.sh
 
-ssh -p 8022 192.168.43.62 
+ssh -p 8022 192.168.43.62
