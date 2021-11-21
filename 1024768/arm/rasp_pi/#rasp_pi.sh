f474602efa37d32fd3b691839a79f7c0f5b6aeb6
PenguinL <diana1997525@gmail.com>
2021-11-21 15:35:32 +0800

    Update #rasp_pi.sh

--- 1024768/arm/rasp_pi/#rasp_pi.sh
+++ 1024768/arm/rasp_pi/#rasp_pi.sh
@@ -10,4 +10,25 @@ git clone https://github.com/AllPlatform/Termux-RaspbianOS
 ##put/replace link&&filename.archive in install.sh
 chmod u+x install.sh && ./install.sh
 unset LD_PRELOAD
+
+nano start-raspbian.sh
+# ..
+# command+=" --kill-on-exit"
+# ..
+# command+=" -b /sys"
+# command+=" -b /data"
+# ..
+# command+=" -b /proc/self/fd/2:/dev/stderr"
+# command+=" -b /proc/self/fd/1:/dev/stdout"
+# command+=" -b /proc/self/fd/0:/dev/stdin"
+# command+=" -b /dev/urandom:/dev/random"
+# command+=" -b /proc/self/fd:/dev/fd"
+# command+=" -b /data/data/com.termux/files/home/rasp_bi/raspbian-fs/proc/fakethings/stat:/proc/stat"
+# command+=" -b /data/data/com.termux/files/home/rasp_bi/raspbian-fs/proc/fakethings/vmstat:/proc/vmstat"
+# command+=" -b /data/data/com.termux/files/home/rasp_bi/raspbian-fs/proc/fakethings/version:/proc/version"
+# ..
+# command+=" -b /sdcard"
+# ..
+# command+=" MOZ_FAKE_NO_SANDBOX=1"
+# ..
 ./start-raspbian.sh
