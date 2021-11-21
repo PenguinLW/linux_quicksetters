a747e0738dda0205ec448d0cbe4fcace3347d061
PenguinL <diana1997525@gmail.com>
2021-11-21 14:11:11 +0800

    Update #rasp_bi.sh

--- 1024768/arm/andronix/#rasp_bi.sh
+++ 1024768/arm/andronix/#rasp_bi.sh
@@ -2,15 +2,16 @@
 #rasp_bi
 
 ##https://www.raspberrypi.com/software/operating-systems/ ##https://github.com/AllPlatform/Termux-RaspbianOS
-##https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-lite.zip ##http://downloads.raspberrypi.org/raspbian_lite/archive/2018-04-19-15:24/root.tar.xz
-##https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf.zip
-##https://downloads.raspberrypi.org/raspios_full_armhf/images/raspios_full_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-full.zip
-
+##http://downloads.raspberrypi.org/raspbian_lite/archive/2018-04-19-15:24/root.tar.xz ##
+##http://downloads.raspberrypi.org/raspios_lite_armhf/archive/2021-11-08-07:44/root.tar.xz ##https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-lite.zip
+##http://downloads.raspberrypi.org/raspios_armhf/archive/2021-11-08-07:41/root.tar.xz ##https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf.zip
+##http://downloads.raspberrypi.org/raspbian_full/root.tar.xz ##
+##http://downloads.raspberrypi.org/raspios_full_armhf/archive/2021-11-08-07:46/root.tar.xz ##https://downloads.raspberrypi.org/raspios_full_armhf/images/raspios_full_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-full.zip
 
 mkdir rasp_bi && cd rasp_bi
 wget https://raw.githubusercontent.com/AllPlatform/Termux-RaspbianOS/master/install.sh
 
-wget 2021-10-30-raspios-bullseye-armhf-lite.tar.xz
+wget http://downloads.raspberrypi.org/raspios_lite_armhf/archive/2021-11-08-07:44/root.tar.xz
 
 nano install.sh
 ##put/replace link&&filename.archive in install.sh
