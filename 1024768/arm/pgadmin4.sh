c1aa899a557f661f26c170cf4fa742bcf6d29e2b
PenguinL <diana1997525@gmail.com>
2021-11-21 08:11:43 +0800

    Create pgadmin4.sh

---
+++ 1024768/arm/pgadmin4.sh
@@ -0,0 +1,9 @@
+##in venv
+#echo {your_password} | sudo -S ls
+##echo 0_penguinl | sudo -S ls
+##echo 0_penguinl | sudo -S /root/pgadmin/bin/pgadmin4
+
+. pgadmin/bin/activate #. venv/bin/activate
+which pgadmin4 && pgadmin4
+
+#http://127.0.0.1:5050/
