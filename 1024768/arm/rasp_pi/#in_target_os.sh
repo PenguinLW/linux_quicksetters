3a133edac59b6dd40419d749dff94f309f81f127
PenguinL <diana1997525@gmail.com>
2021-11-21 14:43:56 +0800

    Update #in_target_os.sh

--- 1024768/arm/rasp_pi/#in_target_os.sh
+++ 1024768/arm/rasp_pi/#in_target_os.sh
@@ -1,9 +1,10 @@
 
 ##https://www.raspberrypi.com/software/operating-systems/ ##https://github.com/AllPlatform/Termux-RaspbianOS
 ##http://downloads.raspberrypi.org/raspbian_lite/archive/2018-04-19-15:24/root.tar.xz ##
-##http://downloads.raspberrypi.org/raspios_lite_armhf/archive/2021-11-08-07:44/root.tar.xz ##https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-lite.zip
+##http://downloads.raspberrypi.org/raspbian_full/root.tar.xz ##http://downloads.raspberrypi.org/raspbian_full/archive/2020-02-14-13:51/root.tar.xz
 ##http://downloads.raspberrypi.org/raspios_armhf/archive/2021-11-08-07:41/root.tar.xz ##https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf.zip
-##http://downloads.raspberrypi.org/raspbian_full/root.tar.xz ##
+
+##http://downloads.raspberrypi.org/raspios_lite_armhf/archive/2021-11-08-07:44/root.tar.xz ##https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-lite.zip
 ##http://downloads.raspberrypi.org/raspios_full_armhf/archive/2021-11-08-07:46/root.tar.xz ##https://downloads.raspberrypi.org/raspios_full_armhf/images/raspios_full_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-full.zip
 
 ##in target os
