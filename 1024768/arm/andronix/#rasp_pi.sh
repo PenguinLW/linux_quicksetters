06b87ffb04e0424f5a4f6f1efad3da32713dcc80
PenguinL <diana1997525@gmail.com>
2021-11-21 11:38:53 +0800

    Update #rasp_pi.sh

--- 1024768/arm/andronix/#rasp_pi.sh
+++ 1024768/arm/andronix/#rasp_pi.sh
@@ -10,7 +10,7 @@
 #mkdir rasp_pi && cd rasp_pi
 git clone https://github.com/AllPlatform/Termux-RaspbianOS #wget https://raw.githubusercontent.com/AllPlatform/Termux-RaspbianOS/master/install.sh
 ##put/replace link&&filename.archive in install.sh
-./install.sh
+chmod u+x install.sh && ./install.sh
 unset LD_PRELOAD
 ./start-raspbian.sh
 
