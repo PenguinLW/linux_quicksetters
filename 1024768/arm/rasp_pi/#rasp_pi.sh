76ed37458fdb3befb545f7cc2970eb92d609cd6a
PenguinL <diana1997525@gmail.com>
2021-11-23 19:26:22 +0800

    Update #rasp_pi.sh

--- 1024768/arm/rasp_pi/#rasp_pi.sh
+++ 1024768/arm/rasp_pi/#rasp_pi.sh
@@ -3,33 +3,13 @@
 
 #mkdir rasp_pi && cd rasp_pi
 
-git clone https://github.com/AllPlatform/Termux-RaspbianOS
-#wget https://raw.githubusercontent.com/AllPlatform/Termux-RaspbianOS/master/install.sh
+wget https://raw.githubusercontent.com/Kovalsky95/linux_quicksetters/master/1024768/arm/rasp_pi/install.sh
 ##wget http://downloads.raspberrypi.org/raspbian_lite/archive/2018-04-19-15:24/root.tar.xz
 
-##put/replace link&&filename.archive in install.sh
+nano install.sh
+##choose/uncomment link&&filename.archive in install.sh
 chmod u+x install.sh && ./install.sh
-unset LD_PRELOAD
 
-#wget https://raw.githubusercontent.com/Kovalsky95/linux_quicksetters/master/1024768/arm/rasp_pi/start-raspbian.sh
 nano start-raspbian.sh
-# ..
-# command+=" --kill-on-exit"
-# ..
-# command+=" -b /sys"
-# command+=" -b /data"
-# ..
-# command+=" -b /proc/self/fd/2:/dev/stderr"
-# command+=" -b /proc/self/fd/1:/dev/stdout"
-# command+=" -b /proc/self/fd/0:/dev/stdin"
-# command+=" -b /dev/urandom:/dev/random"
-# command+=" -b /proc/self/fd:/dev/fd"
-# command+=" -b /data/data/com.termux/files/home/rasp_pi/raspbian-fs/proc/fakethings/stat:/proc/stat"
-# command+=" -b /data/data/com.termux/files/home/rasp_pi/raspbian-fs/proc/fakethings/vmstat:/proc/vmstat"
-# command+=" -b /data/data/com.termux/files/home/rasp_pi/raspbian-fs/proc/fakethings/version:/proc/version"
-# ..
-# command+=" -b /sdcard"
-# ..
-# command+=" MOZ_FAKE_NO_SANDBOX=1"
 # ..
 ./start-raspbian.sh
