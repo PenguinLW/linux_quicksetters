419fab7b24ee218755fd8859eafdd92197e21f3b
PenguinL <diana1997525@gmail.com>
2021-12-07 15:17:36 +0800

    Add files via upload

---
+++ 1024768/what.bat
@@ -0,0 +1,65 @@
+@echo off
+set path=%path%;%userprofile%\Documents\;%userprofile%\Desktop\
+prompt ??
+title ??
+cls
+cd
+chdir
+set
+tasklist
+
+rem time
+rem date
+
+tree
+dir
+rem copy
+help xcopy
+help robocopy
+help del
+help erase
+help mk && help mkdir
+help rd && help rmdir
+hel ren && help rename
+help replace
+help comp
+help fc
+help assoc
+help attrib
+
+help find
+help findstr
+help path
+help subst
+help sort
+help mklink
+help setlocal
+help fsutil
+rem more
+help gpresult
+help mode
+help graftabl
+ver
+systeminfo
+
+rem start
+help sc
+help schtasks
+help taskkill
+help type
+help break
+help verify
+help bcdedit
+
+help calcs
+help chkdsk
+help chkntfs
+
+help color
+help compact
+help convert
+
+help cmd
+echo %userprofile%
+help help
+help
