4b22cc055565b1929b2e57e9df272a7ee9cb8e27
Kovalsky95 <diana1997525@gmail.com>
2021-12-07 19:09:54 +0800

    PenguinL

--- get_all.sh
+++ get_all.sh
@@ -1,4 +1,5 @@
 git clone -o linux_quicksetters https://github.com/Kovalsky95/linux_quicksetters.git
 #rm -dfr Programm && mkdir Programm
-mv linux_quicksetters/* Programm/
+#mv linux_quicksetters/* Programm/
+cp -R linux_quicksetters/* Programm/
 rm -dfr linux_quicksetters
