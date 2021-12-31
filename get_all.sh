cdf48b8e79a5052b3a6cc3beb06c35bbde9939cc
PenguinL <diana1997525@gmail.com>
2021-12-31 13:10:21 +0800

    Update get_all.sh

--- get_all.sh
+++ get_all.sh
@@ -1,5 +1,6 @@
 git clone -o linux_quicksetters https://github.com/Kovalsky95/linux_quicksetters.git
 #rm -dfr Programm && mkdir Programm
 #mv linux_quicksetters/* Programm/
-cp -R linux_quicksetters/* Programm/
-rm -dfr linux_quicksetters
+#cp -R linux_quicksetters/* Programm/
+#rm -dfr linux_quicksetters
+ln -s linux_quicksetters/ Programm
