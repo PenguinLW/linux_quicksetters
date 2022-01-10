8d7ed2b301a12972448c70703035975fa405f3c7
PenguinL <diana1997525@gmail.com>
2022-01-10 22:41:28 +0800

    Update set_go.sh

--- 1024768/arm/exatorrent/set_go.sh
+++ 1024768/arm/exatorrent/set_go.sh
@@ -5,6 +5,9 @@
 wget https://go.dev/dl/go1.17.6.linux-armv6l.tar.gz --no-check-certificate #https://golang.org/dl/go1.17.1.linux-armv6l.tar.gz #https://golang.org/dl/go1.17.1.linux-386.tar.gz #https://golang.org/dl/go1.17.1.linux-amd64.tar.gz #https://golang.org/dl/go1.17.1.linux-arm64.tar.gz
 tar -xvzf go1.17.6.linux-armv6l.tar.gz && rm -fr go1.17.6.linux-armv6l.tar.gz
 
+#wget https://go.dev/dl/go1.17.6.linux-arm64.tar.gz --no-check-certificate
+#tar -xvzf go1.17.6.linux-arm64.tar.gz && rm -fr go1.17.6.linux-arm64.tar.gz
+
 update-alternatives --install /usr/bin/go go /root/go/bin/go 1
 update-alternatives --set go /root/go/bin/go
 update-alternatives --install /usr/bin/gofmt gofmt /root/go/bin/gofmt 1
