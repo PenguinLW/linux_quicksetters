cedf2a8acfa61cfd1bf28d33ea931eab7d8e1782
PenguinL <diana1997525@gmail.com>
2022-01-10 21:25:34 +0800

    Update set_go.sh

--- 1024768/arm/exatorrent/set_go.sh
+++ 1024768/arm/exatorrent/set_go.sh
@@ -3,7 +3,7 @@
 #https://www.e-tinkers.com/2019/06/better-way-to-install-golang-go-on-raspberry-pi/
 
 wget https://go.dev/dl/go1.17.6.linux-armv6l.tar.gz --no-check-certificate #https://golang.org/dl/go1.17.1.linux-armv6l.tar.gz #https://golang.org/dl/go1.17.1.linux-386.tar.gz #https://golang.org/dl/go1.17.1.linux-amd64.tar.gz #https://golang.org/dl/go1.17.1.linux-arm64.tar.gz
-tar -xvzf go1.17.1.linux-armv6l.tar.gz && rm -fr go1.17.1.linux-armv6l.tar.gz
+tar -xvzf go1.17.6.linux-armv6l.tar.gz && rm -fr go1.17.6.linux-armv6l.tar.gz
 
 update-alternatives --install /usr/bin/go go /root/go/bin/go 1
 update-alternatives --set go /root/go/bin/go
