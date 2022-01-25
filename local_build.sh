d4c2cc4c360a2b482358f045c4a13b8fb227df9d
Kovalsky95 <diana1997525@gmail.com>
2022-01-26 02:32:29 +0800

    PenguinL

--- local_build.sh
+++ local_build.sh
@@ -1,1 +1,4 @@
-cp -r /home/penguinl/Documentos/PycharmProjects/linux_quicksetters/* /home/penguinl/Programm/
+##cp -r /home/penguinl/Documentos/PycharmProjects/linux_quicksetters/* /home/penguinl/Programm/
+tmp=`pwd`
+cd ~
+ln -s $tmp Programm
