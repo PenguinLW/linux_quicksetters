8777a8b8c86c78d389a05f754f722899f79474d6
PenguinLW <diana1997525@gmail.com>
2023-04-19 13:50:32 +0000

    PenguinL

--- update_python.sh
+++ update_python.sh
@@ -5,54 +5,25 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 #sudo apt-get install python3.7 -y
 #sudo apt-get install python3.8 -y
 sudo apt-get install python3.9 -y
-#sudo apt-get install python3.11 -y
 which python3.5
 which python3.6
 which python3.7
 which python3.8
 which python3.9
-which python3.11
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
 #sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
 #sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 3
 #sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 4
-#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 5
-#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
-sudo apt-get install python3.5-dev -y
-sudo apt-get install python3.6-dev -y
-sudo apt-get install python3.7-dev -y
-sudo apt-get install python3.8-dev -y
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 5
+
+#sudo apt-get install python3.5-dev -y
+#sudo apt-get install python3.6-dev -y
+#sudo apt-get install python3.7-dev -y
+#sudo apt-get install python3.8-dev -y
 sudo apt-get install python3.9-dev -y
-#sudo apt-get install python3.11-dev -y
 
-sudo apt-get install python3.5-venv -y
-sudo apt-get install python3.6-venv -y
-sudo apt-get install python3.6-venv -y
-sudo apt-get install python3.8-venv -y
+#sudo apt-get install python3.5-venv -y
+#sudo apt-get install python3.6-venv -y
+#sudo apt-get install python3.6-venv -y
+#sudo apt-get install python3.8-venv -y
 sudo apt-get install python3.9-venv -y
-#sudo apt-get install python3.11-venv -y
-
-
-sudo apt-get upgrade python3.6 -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
-#set pip?pip3, make scrcpy ..
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 3
-
-
-sudo apt-get install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
-
-sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-pil python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev -y
-sudo apt-get install python-setuptools
-
-sudo apt-get install python3-pip python3.7-dev
-sudo apt-get install python3.7
-sudo apt-get install python3-dev python3-setuptools python3-wheel -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 5
-
-#sudo systemctl restart display-manager
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 6
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 7
-#sudo apt-get install cinnamon cinnamon-common --reinstall
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 8
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 9
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 10
