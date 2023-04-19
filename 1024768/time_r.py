61c523705c591888374ccca397b0fd361a28d938
PenguinLW <diana1997525@gmail.com>
2023-04-19 03:21:01 +0000

    PenguinL

--- 1024768/time_r.py
+++ 1024768/time_r.py
@@ -1,24 +1,28 @@
-from time import time, ctime;
-from datetime import datetime
+def time_use():
+    from time import time, ctime;
+    #time() - time in seconds since the epoch; ctime() - local time.
+    ta = ctime(time())
 
-#time() - time in seconds since the epoch; ctime() - local time.
-ta = ctime(time())
+    #
+    input("ДА! Вот просто стою - и просто жду. Это ТЫ ничего не понимаешь!");
 
-#
-input("ДА! Вот просто стою - и просто жду. Это ТЫ ничего не понимаешь!");
+    #
+    tb = ctime(time());
 
-#
-tb = ctime(time());
+    print(
+        "Момент начала измерения:\t{:s}\nИзмерение завершено:\t{:s}".format(ta, tb, )
+    );
 
-print(
-    "Момент начала измерения:\t{:s}\nИзмерение завершено:\t{:s}".format(ta, tb, )
-);
 
-print(
-    "{:s}\n{:s}\n{:s}\n{:s}\n{:s}".format(
-        str(datetime.now()),
-        str(datetime.now().hour),
-        str(datetime.now().minute),
-        str(datetime.now().second),
-        str(datetime.now().microsecond)
-));
+def datetime_use():
+    from datetime import datetime
+    print(
+        "{:s}\n{:s}\n{:s}\n{:s}\n{:s}".format(
+            str(datetime.now()),
+            str(datetime.now().hour),
+            str(datetime.now().minute),
+            str(datetime.now().second),
+            str(datetime.now().microsecond)
+    ));
+
+datetime_use();
