8603bafc3b60e03c027ab5a2d59c363de253b60d
PenguinLW <diana1997525@gmail.com>
2023-04-21 09:10:42 +0000

    PenguinL

--- 1024768/time_r.py
+++ 1024768/time_r.py
@@ -1,29 +1,3 @@
-def time_use():
-    from time import time, ctime;
-    #time() - time in seconds since the epoch; ctime() - local time.
-    ta = ctime(time())
-
-    #
-    input("ДА! Вот просто стою - и просто жду. Это ТЫ ничего не понимаешь!");
-
-    #
-    tb = ctime(time());
-
-    print(
-        "Момент начала измерения:\t{:s}\nИзмерение завершено:\t{:s}".format(ta, tb, )
-    );
-
-
-def datetime_use():
-    from datetime import datetime
-    print(
-        "{:s}\n{:s}\n{:s}\n{:s}\n{:s}".format(
-            str(datetime.now()),
-            str(datetime.now().hour),
-            str(datetime.now().minute),
-            str(datetime.now().second),
-            str(datetime.now().microsecond)
-    ));
 def datetime_count():
     from datetime import datetime
     past_time = list();
