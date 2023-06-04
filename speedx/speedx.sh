fd3a714f65eb92c5a9d799e319a6acfe7d59a734
PenguinLW <diana1997525@gmail.com>
2023-06-04 17:57:53 +0800

    Create speedx.sh

---
+++ speedx/speedx.sh
@@ -0,0 +1,7 @@
+#https://snapcraft.io/install/speedx/manjaro
+sudo pacman -Ss snapd
+sudo pacman -S snapd
+
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install speedx
