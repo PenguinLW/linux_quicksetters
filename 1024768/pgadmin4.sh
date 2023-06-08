6c3b6c14d9ae5a89f565e8276d1eddd11315ca34
PenguinLW <diana1997525@gmail.com>
2023-06-08 17:59:57 +0800

    PenguinL

--- 1024768/pgadmin4.sh
+++ 1024768/pgadmin4.sh
@@ -1,9 +1,7 @@
 ##in venv
-#echo {your_password} | sudo -S ls
-echo 0_penguinl | sudo -S ls
-echo 0_penguinl | sudo -S /home/penguinl/Documentos/PycharmProjects/linux_quicksetters/venv/bin/pgadmin4
+#echo {your_password} | sudo -S ls - al
 
-#. venv/bin/activate
-#which pgadmin4 && pgadmin4
+cd ~ && source venv/bin/activate
+which pgadmin4 && pgadmin4
 
 #http://127.0.0.1:5050/
