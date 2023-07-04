6b2741869ba6f1a2c54a05e6cf9dfe1bfa50a8e7
PenguinLW <diana1997525@gmail.com>
2023-07-04 17:00:39 +0800

    Update and rename new_python3_venv.sh to new_python3_venv.sh

--- update_python/new_python3_venv.sh
+++ update_python/new_python3_venv.sh
@@ -1,5 +1,9 @@
 #https://docs.python.org/3/library/venv.html
 sudo apt-get install python3-venv -y
+
+#in current dir
 python3 -m venv venv
 #chmod u+x venv/bin/*
-. venv/bin/activate
+
+#cd ~ && ln -s /{path_to}/{cur_dir}/venv
+source ~/venv/bin/activate
