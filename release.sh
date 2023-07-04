8a0a29717533cf8f6997f9dd00232c3d384eecd9
PenguinLW <diana1997525@gmail.com>
2023-07-04 18:35:46 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -3,6 +3,8 @@
 #pip install -r req.txt
 #pip freeze > req.txt
 chmod u+x *.sh *.py
+
+#git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/linux_quicksetters
 #git clone -o linux_quicksetters https://github.com/PenguinLW/linux_quicksetters.git
 #git config --global user.email "diana1997525@gmail.com"
 #git config --global user.name "PenguinLW"
@@ -10,6 +12,7 @@ chmod u+x *.sh *.py
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
+
 git pull --all
 git add .
 git commit -m PenguinL
