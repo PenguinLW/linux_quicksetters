61017a62b3317bf2a0143235e0fc451dfc389ce8
PenguinLW <diana1997525@gmail.com>
2023-07-09 00:54:39 +0800

    PenguinL

--- update_python/manjaro_postgresql_psql.sh
+++ update_python/manjaro_postgresql_psql.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-#sudo pacman -S postgresql
+sudo pacman -S postgresql
 #sudo systemctl is-active postgresql
 #sudo systemctl is-enabled postgresql
 #sudo systemctl status postgresql
 #sudo pg_isready
+#sudo -u postgres psql
+
+sudo -su postgres
+#in postgres, which this open
+initdb /var/lib/postgres/data
+systemctl start postgresql.service
+exit
+
+#in terminal
+sudo systemctl restart postgresql.service
+
+#in pgadmin4: add server with custom name, "localhost" address, username "postgres", no password
