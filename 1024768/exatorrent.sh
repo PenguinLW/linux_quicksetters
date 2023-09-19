c0cd926f4aca79b682e5f9472a4436f9dc098ae5
PenguinLW <diana1997525@gmail.com>
2023-09-19 23:54:20 +0800

    Update exatorrent.sh

--- 1024768/exatorrent.sh
+++ 1024768/exatorrent.sh
@@ -1,6 +1,20 @@
+##!/bin/bash
+#ulimit -Sv 7000000 #5000000
+#./exatorrent*linux*
+#
+##ps -ax | grep exatorrent-linux-amd64 #ps -axuef exatorrent
+##killall exatorrent-linux-amd64
 #!/bin/bash
-ulimit -Sv 7000000 #5000000
-./exatorrent*linux*
+ulimit -Sv 7000000
+#./exatorrent*linux*
+#./exatorrent-linux-amd64
 
-#ps -ax | grep exatorrent-linux-amd64 #ps -axuef exatorrent
+#./Programm/exatorrent/local_manual/exatorrent-linux-amd64
+
+##cd /usr/bin/
+##sudo ln -s /{path}/{to_directory}/{local}/{build}/exatorrent/local_manual/exatorrent.sh
+##exatorrent
+
+~/Programm/exatorrent/local_manual/exatorrent-linux-amd64
+#ps -axuef exatorrent
 #killall exatorrent-linux-amd64
