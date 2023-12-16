5e8b398919304cdeb4fc55147fa352e6ccd4685a
PenguinLW <diana1997525@gmail.com>
2023-12-16 16:53:10 +0800

    Create add_nvidia.h

---
+++ other_set/add_nvidia.h
@@ -0,0 +1,22 @@
+##https://wiki.manjaro.org/index.php/Configure_NVIDIA_(non-free)_settings_and_load_them_on_Startup
+##https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-manjaro-linux
+
+
+inxi -G
+mhwd -li
+
+sudo mhwd -a pci nonfree 0300
+mhwd -li
+  
+sudo nvidia-settings
+
+sudo mhwd-gpu --setmod nvidia --setxorg /etc/X11/mhwd.d/nvidia.conf
+
+nvidia-settings
+
+gedit ~/.xinitrc
+##put the:
+##nvidia-settings --load-config-only
+##exec $(get_session)
+
+
