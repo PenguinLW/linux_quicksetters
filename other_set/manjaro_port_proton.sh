25d2131e4fd63a7807a52b31c26acb0e4995e1f6
PenguinLW <diana1997525@gmail.com>
2024-03-25 14:33:57 +0800

    PenguinL

--- other_set/manjaro_port_proton.sh
+++ other_set/manjaro_port_proton.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 pamac build portproton
+
+'''
+sudo pacman -Syu bash icoutils bubblewrap zstd cabextract tar openssl gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font lsb-release nss xorg-xrandr vulkan-driver vulkan-icd-loader lsof lib32-freetype2 lib32-libgl lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lib32-libgpg-error lib32-nss lib32-vulkan-driver lib32-vulkan-icd-loader lib32-gamemode lib32-openssl
+'''
+
+'''
+sudo pacman -Syu lib32-nvidia-utils
+'''
