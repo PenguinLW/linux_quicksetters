c619536774f989a1e4856f3091b7da38da8d4fb8
PenguinLW <diana1997525@gmail.com>
2024-07-06 21:56:56 +0800

    Create manjaro_theme_fix.sh

---
+++ other_set/manjaro_theme_fix.sh
@@ -0,0 +1,4 @@
+#gi.repository.GLib.GError: gtk-icon-theme-error-quark: Значок «webapp-manager» не присутствует в теме Papirus-Dark-Maia (0)
+stat /usr/share/applications/mimeinfo.cache
+sudo update-mime-database /usr/share/mime
+sudo gdk-pixbuf-query-loaders --update-cache
