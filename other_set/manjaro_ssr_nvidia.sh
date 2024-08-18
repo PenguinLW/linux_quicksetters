931416acb78cc32fb0bc9bed20d0f0629bcd38d2
PenguinLW <diana1997525@gmail.com>
2024-08-18 13:53:57 +0800

    PenguinL

---
+++ other_set/manjaro_ssr_nvidia.sh
@@ -0,0 +1,12 @@
+sudo pacman -S ffmpeg
+sudo pacman -S xbindkeys
+
+sudo pacman -S libnotify
+
+##https://github.com/MAPReiff/ShadowRePlay-Linux
+##pamac build shadowreplay-git
+
+
+##https://www.maartenbaert.be/simplescreenrecorder/
+pamac build simplescreenrecorder-git
+#pamac build lib32-simplescreenrecorder
