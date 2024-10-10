f663767f283b451b13ce1cee36954818d882bac8
PenguinLW <diana1997525@gmail.com>
2024-10-10 11:07:27 +0800

    PenguinL

--- 1024768/float_wind.sh
+++ 1024768/float_wind.sh
@@ -1,2 +1,12 @@
 #adb -s <your-device-id> shell settings put global enable_freeform_support 1
 #adb -s <your-device-id>:5555 shell settings put global enable_freeform_support 1
+
+#adb shell pm grant <your-package-name> android.permission.SYSTEM_ALERT_WINDOW
+##adb shell pm grant com.vietbm.s9navigation android.permission.SYSTEM_ALERT_WINDOW
+##adb shell pm grant nu.nav.bar android.permission.SYSTEM_ALERT_WINDOW
+##adb shell pm grant com.githib.ericytsang.multiwindow.app android.permission.SYSTEM_ALERT_WINDOW
+##adb shell pm grant net.dinglisch.android.taskerm android.permission.SYSTEM_ALERT_WINDOW
+
+##adb shell pm grant com.arlosoft.macrodroid android.permission.SYSTEM_ALERT_WINDOW
+##adb shell pm grant tv.twitch.android android.permission.SYSTEM_ALERT_WINDOW
+##adb shell pm grant com.lbe.parallel.ui android.permission.SYSTEM_ALERT_WINDOW
