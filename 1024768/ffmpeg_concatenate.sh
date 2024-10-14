ca8ee3c8f813f6ec125c36b3a355126417a382f7
PenguinLW <diana1997525@gmail.com>
2024-10-14 15:09:52 +0800

    PenguinL

--- 1024768/ffmpeg_concatenate.sh
+++ 1024768/ffmpeg_concatenate.sh
@@ -1,5 +1,5 @@
 
 ##ls *.mkv> out_list.txt
-printf "file '%s'\n" *.mkv> out_list.txt
+printf "file '%s'\n" *_out.mkv> out_list.txt
 ##ffmpeg -f concat -i out_list.txt output.mkv
 ffmpeg -f concat -i out_list.txt -c copy output.mkv
