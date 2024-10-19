c29b92d6da59a56ce81a67af409ce7e36b01797c
PenguinLW <diana1997525@gmail.com>
2024-10-19 19:26:21 +0800

    PenguinL

--- 1024768/ffmpeg_compress.sh
+++ 1024768/ffmpeg_compress.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ffmpeg -i *.mp4 -vcodec libx265 -crf 28 out.mp4
+ffmpeg -i *.mkv -vcodec libx265 -crf 28 out.mkv
 
 #ffmpeg -ss 00:00:05 -t 01:22:46 -i *.mkv -vcodec libx265 -crf 28 out.mkv
 
