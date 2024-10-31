4edd2d940a6e8f1747ea75174d2aa86e0074eb5b
PenguinLW <diana1997525@gmail.com>
2024-10-31 15:36:09 +0800

    PenguinL

--- 1024768/ffmpeg_crop.sh
+++ 1024768/ffmpeg_crop.sh
@@ -1,7 +1,8 @@
-#"crop=crop_w:crop_h:crop_x:crop_y"
-ffmpeg -ss 00:00:05 -t 00:00:33 -i in.mp4 -vf "crop=300:450:800:250" out.mp4
-ffmpeg -i *.mkv -vf "crop=600:800:650:250" out.mkv
-ffmpeg -i *.mkv -vf "crop=600:800:650:250" out.mp4
+##"crop=crop_w:crop_h:crop_x:crop_y"
+##ffmpeg -ss 00:00:05 -t 00:00:33 -i in.mp4 -vf "crop=300:450:800:250" out.mp4
+
+#ffmpeg -i *.mkv -vf "crop=600:800:650:250" out.mkv
+#ffmpeg -i *.mkv -vf "crop=600:800:650:250" out.mp4
 ffmpeg -i *.mkv -vf "crop=750:950:650:100" out.mp4
 
 #ffmpeg -ss 00:00:05 -t 01:22:46 -i *.mkv out.mkv
