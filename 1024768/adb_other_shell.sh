41cadd8a5a8b90a8a487f6ec7743436bfac85ef8
PenguinLW <diana1997525@gmail.com>
2024-12-18 20:32:33 +0800

    PenguinL

--- 1024768/adb_other_shell.sh
+++ 1024768/adb_other_shell.sh
@@ -2,4 +2,4 @@ adb shell pm list packages -d
 
 adb shell am switch-user 0 #10 #11 #12
 adb shell settings put global --user 0 development_settings_enabled 1 
-#adb shell am start --user 0 -n com.android.settings/.DevelopmentSettings
+adb shell am start --user 0 -n com.android.settings/.Settings$DeveloperSettingsActivity #adb shell am start --user 0 -n com.android.settings/.DevelopmentSettings
