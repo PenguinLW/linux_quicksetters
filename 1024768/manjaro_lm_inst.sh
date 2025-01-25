e7ed9d3ad3c3ab3d2a25fbeea01a378594356105
PenguinLW <diana1997525@gmail.com>
2025-01-25 16:58:49 +0800

    PenguinL

--- 1024768/manjaro_lm_inst.sh
+++ 1024768/manjaro_lm_inst.sh
@@ -3,5 +3,7 @@ sudo pamac build lmstudio --no-confirm
 
 sudo ln -s /media/penguinl/PenguinL/Programm/LM_STUDIO/LM-Studio-0.3.8-4-x64.AppImage /usr/bin/lm
 whuch lm
+ln -s /media/penguinl/PenguinL/Programm/LM_STUDIO/.lmstudio 
 
-
+#hugginface.co
+#https://github.com/easydiffusion/easydiffusion #https://github.com/easydiffusion/easydiffusion/releases/download/v3.0.9/Easy-Diffusion-Linux.zip
