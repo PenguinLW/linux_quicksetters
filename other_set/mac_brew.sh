61bbf7900e172763f3bbb995cc3a18c28e8aaf80
PenguinLW <diana1997525@gmail.com>
2025-03-17 17:18:59 +0800

    Update mac_brew.sh

--- other_set/mac_brew.sh
+++ other_set/mac_brew.sh
@@ -3,6 +3,7 @@ brew install qpdf
 curl ifconfig.me
 #109.163.217.14
 
-
+python3 -m venv venv
+source venv/bin/activate
 pip install --upgrade pip setuptools wheel
 pip install img2pdf Pillow opencv-contrib-python
