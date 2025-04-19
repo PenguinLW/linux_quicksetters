c835fbbda06bae7816916731a9a29f5528a00de6
PenguinLW <diana1997525@gmail.com>
2025-04-19 16:15:18 +0800

    Update manjaro_ssr_nvidia.sh

--- other_set/manjaro_ssr_nvidia.sh
+++ other_set/manjaro_ssr_nvidia.sh
@@ -10,3 +10,13 @@ sudo pacman -S libnotify
 ##https://www.maartenbaert.be/simplescreenrecorder/
 pamac build simplescreenrecorder-git
 #pamac build lib32-simplescreenrecorder
+
+echo $XDG_SESSION_TYPE
+
+sudo pacman -S wf-recorder
+sudo pacman -S obs-studio
+
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install blue-recorder
