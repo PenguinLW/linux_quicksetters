826f06a43da68b63736cf0f7cf5aee97b7aefbe1
PenguinLW <diana1997525@gmail.com>
2025-05-22 02:17:49 +0800

    PenguinLW

--- other_set/manjaro_bitrix24.sh
+++ other_set/manjaro_bitrix24.sh
@@ -4,3 +4,16 @@ sudo pacman -Syy
 pamac search bitrix
 pamac search bitrix24
 pamac build bitrix24
+
+#ping aur.archlinux.org
+#sudo pacman -S pamac
+#sudo pacman -S pamac-aur
+
+#mkdir tmp && cd tmp
+#git clone https://aur.archlinux.org/bitrix24.git
+#cd bitrix24
+#makepkg -si
+
+#sudo pacman -S yay
+#yay -S bitrix24
+#yay -S google-chrome
