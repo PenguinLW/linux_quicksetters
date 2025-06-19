f0db1f0a5ac6e8cd705de0d6eb6098fce15b5ca9
PenguinLW <diana1997525@gmail.com>
2025-06-20 01:30:52 +0800

    PenguinLW

--- 1024768/empty_cache.sh
+++ 1024768/empty_cache.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+#sudo rm -fr /var/cache/pacman/pkg/*
+#sudo rm -fr /var/cache*
+
+sudo pacman -Sc
+sudo pacman -S pacman-contrib
+sudo paccache -r
+sudo paccache -rk1
+sudo paccache -rk0
+
+sudo systemctl start systemd-tmpfiles-clean.service
+yay -Sc
+paru -Sc
+
+sudo resolvectl flush-caches
+df -h /
+du -hs /var/cache/pacman/pkg
