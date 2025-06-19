9f7d6b6615acf9a5b0d5b21a4ab2aeaf7b9534b4
PenguinLW <diana1997525@gmail.com>
2025-06-20 03:16:24 +0800

    PenguinLW

--- other_set/manjaro_port_proton.sh
+++ other_set/manjaro_port_proton.sh
@@ -8,3 +8,64 @@ sudo pacman -Syu bash icoutils bubblewrap zstd cabextract tar openssl gamemode d
 '''
 sudo pacman -Syu lib32-nvidia-utils
 '''
+
+#fix err
+touch /tmp/PortProton/portproton.lock 
+sudo pacman -S gamescope
+sudo pacman -S --needed \
+    wine-staging \
+    winetricks \
+    vulkan-icd-loader \
+    lib32-vulkan-icd-loader \
+    giflib \
+    lib32-giflib \
+    libpng \
+    lib32-libpng \
+    libldap \
+    lib32-libldap \
+    gnutls \
+    lib32-gnutls \
+    mpg123 \
+    lib32-mpg123 \
+    openal \
+    lib32-openal \
+    v4l-utils \
+    lib32-v4l-utils \
+    libpulse \
+    lib32-libpulse \
+    alsa-plugins \
+    lib32-alsa-plugins \
+    alsa-lib \
+    lib32-alsa-lib \
+    libjpeg-turbo \
+    lib32-libjpeg-turbo \
+    libxcomposite \
+    lib32-libxcomposite \
+    libxinerama \
+    lib32-libxinerama \
+    ncurses \
+    lib32-ncurses \
+    opencl-icd-loader \
+    lib32-opencl-icd-loader \
+    libxslt \
+    lib32-libxslt \
+    libva \
+    lib32-libva \
+    gtk3 \
+    lib32-gtk3 \
+    gst-plugins-base-libs \
+    lib32-gst-plugins-base-libs
+
+rm -rf ~/PortProton/data/prefixes/DEFAULT
+chmod -R 755 ~/PortProton
+#yay -Syu portproton  # или paru -Syu portproton
+
+cd ~/PortProton
+./portproton -debug 2>&1 | tee portproton.log
+
+#nano portproton.log
+
+uname -r
+df -h ~/
+nvidia-smi
+#vulkaninfo | grep "GPU id"
